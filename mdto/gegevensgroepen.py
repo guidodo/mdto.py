75827d0ae3b0497fad816bc889ca8b8f35ebadaf
Rijnder Wever <rien334@gmail.com>
2024-12-06 16:29:13 +0100

    feat: ability to import all *Gegevens classes
    This is mostly so that users can `from mdto.gegevensgroepen import *`,
    and have all commonly used classes immediatly available.

---
+++ mdto/gegevensgroepen.py
@@ -0,0 +1,32 @@
+from .mdto import (
+    IdentificatieGegevens,
+    VerwijzingGegevens,
+    BegripGegevens,
+    TermijnGegevens,
+    ChecksumGegevens,
+    BeperkingGebruikGegevens,
+    DekkingInTijdGegevens,
+    EventGegevens,
+    RaadpleeglocatieGegevens,
+    GerelateerdInformatieobjectGegevens,
+    BetrokkeneGegevens,
+    Informatieobject,
+    Bestand,
+)
+
+# Expose desired classes
+__all__ = [
+    "IdentificatieGegevens",
+    "VerwijzingGegevens",
+    "BegripGegevens",
+    "TermijnGegevens",
+    "ChecksumGegevens",
+    "BeperkingGebruikGegevens",
+    "DekkingInTijdGegevens",
+    "EventGegevens",
+    "RaadpleeglocatieGegevens",
+    "GerelateerdInformatieobjectGegevens",
+    "BetrokkeneGegevens",
+    "Informatieobject",
+    "Bestand",
+]
